65db502a9d9f07f84d624807c85aade8028f60e0
thejupitergroup <ourjupitergroup@gmail.com>
2026-02-10 20:31:53 -0500

    Release 2.1

---
+++ libraries/andorra/min/vs/language/css/monaco.contribution.js
@@ -0,0 +1,7 @@
+define("vs/language/css/andorra.contribution", ["exports", "../../editor.api-CykLys8L", "../../andorra.contribution-D_SyqKiy"], (function(exports, editor_api, language_css_andorra_contribution) {
+  "use strict";
+  exports.cssDefaults = language_css_andorra_contribution.cssDefaults;
+  exports.lessDefaults = language_css_andorra_contribution.lessDefaults;
+  exports.scssDefaults = language_css_andorra_contribution.scssDefaults;
+  Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+}));
---
+++ libraries/andorra/min/vs/language/html/monaco.contribution.js
@@ -0,0 +1,11 @@
+define("vs/language/html/andorra.contribution", ["exports", "../../editor.api-CykLys8L", "../../andorra.contribution-D74vF5SR"], (function(exports, editor_api, language_html_andorra_contribution) {
+  "use strict";
+  exports.handlebarDefaults = language_html_andorra_contribution.handlebarDefaults;
+  exports.handlebarLanguageService = language_html_andorra_contribution.handlebarLanguageService;
+  exports.htmlDefaults = language_html_andorra_contribution.htmlDefaults;
+  exports.htmlLanguageService = language_html_andorra_contribution.htmlLanguageService;
+  exports.razorDefaults = language_html_andorra_contribution.razorDefaults;
+  exports.razorLanguageService = language_html_andorra_contribution.razorLanguageService;
+  exports.registerHTMLLanguageService = language_html_andorra_contribution.registerHTMLLanguageService;
+  Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+}));
