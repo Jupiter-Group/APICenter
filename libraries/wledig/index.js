cf6d814a4e139450d63a1c868f05337b861e4efb
thejupitergroup <ourjupitergroup@gmail.com>
2026-02-15 11:05:02 -0500

    Release 3.1

--- libraries/wledig/index.js
+++
@@ -1,5 +0,0 @@
-// index.js
-import './node_modules/@material/web/button/filled-button.js';
-import './node_modules/@material/web/button/filled-button.js';
-import './node_modules/@material/web/button/outlined-button.js';
-import './node_modules/@material/web/checkbox/checkbox.js';
