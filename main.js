614fbfd3099c9fba8d4165fd907e157f37607379
thejupitergroup <ourjupitergroup@gmail.com>
2026-02-15 11:18:48 -0500

    Release 3.5

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Load components (from your own server)
 import "https://apicenter.pages.dev/libraries/wledig/button/filled-button.js";
-import "./vendor/material-web/textfield/filled-text-field.js";
+import "https://apicenter.pages.dev/libraries/wledig/textfield/filled-text-field.js";
 
 // App logic
 const btn = document.getElementById("btn");
